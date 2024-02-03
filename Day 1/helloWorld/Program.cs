ff18748427d6ad1026d62a98695779ac81b9088d
ishworX <git.ishwor@gmail.com>
2024-02-02 23:56:30 -0700

    learning difference between Console.Write and Writeline

--- Day 1/helloWorld/Program.cs
+++ Day 1/helloWorld/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, I am a C# programmer.");
+Console.WriteLine("Hello, I am a C# programmer.");
+Console.Write("First");
+Console.Write("Second");
 

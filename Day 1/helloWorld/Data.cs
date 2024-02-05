fe34b364fad5ffd5d6e680cdd2b9b01042015d9b
ishworX <git.ishwor@gmail.com>
2024-02-04 23:41:46 -0700

    compiled sucessfully

--- Day 1/helloWorld/Data.cs
+++ Day 1/helloWorld/Data.cs
@@ -6,8 +6,8 @@ namespace Coding.Exercise
         public void RunExercise() {
 
             Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>() {
-                {"cheese", 5.99},
-                {"carrrots", 2.99}
+                {"cheese", 5.99m},
+                {"carrots", 2.99m}
             }; 
 
             Console.WriteLine(itemPrices["cheese"].GetType());

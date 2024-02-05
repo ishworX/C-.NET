59bb0862b130f8f79f09418c30e42e5765c6d8a6
ishworX <git.ishwor@gmail.com>
2024-02-04 23:40:15 -0700

    writing an dictionary

--- Day 1/helloWorld/Data.cs
+++ Day 1/helloWorld/Data.cs
@@ -5,6 +5,11 @@ namespace Coding.Exercise
     public class Exercise {
         public void RunExercise() {
 
+            Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>() {
+                {"cheese", 5.99},
+                {"carrrots", 2.99}
+            }; 
+
             Console.WriteLine(itemPrices["cheese"].GetType());
             Console.WriteLine(itemPrices["cheese"]);
             Console.WriteLine(itemPrices["carrots"]);
